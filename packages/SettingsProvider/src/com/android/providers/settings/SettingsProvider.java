2c0106424f61407178508a3e046d05d1bb41e310
pdxt <pdxt@google.com>
2026-01-07 09:28:51 +0000

    [SettingsProvider] Add redactedValue to @Readable
    We add a new annnotation field to @Readable that allows specifying a default value to be returned. We also try to make this approach more efficient so as to not cause any regression in metrics.
    
    Test: atest android.appsecurity.cts.ReadableSettingsFieldsTest
    Bug: 440232200
    Flag: android.provider.enable_redacted_value_for_readable
    
    Change-Id: I3ec8fddcc8494367964af8c62399ea938b7a5c72
    Signed-off-by: Chara <bionichuman07@gmail.com>
    Signed-off-by: Chara <bunnypad@murena.io>
    Signed-off-by: Chara <92406354+diverselightchara@users.noreply.github.com>

--- packages/SettingsProvider/src/com/android/providers/settings/SettingsProvider.java
+++ packages/SettingsProvider/src/com/android/providers/settings/SettingsProvider.java
@@ -347,31 +347,40 @@ public class SettingsProvider extends ContentProvider {
         Settings.System.getCloneFromParentOnValueSettings(sSystemCloneFromParentOnDependency);
     }
 
+    private static final boolean ENABLE_REDACTED_VALUE_FOR_READABLE =
+            android.provider.Flags.enableRedactedValueForReadable();
+
     private static final Set<String> sAllSecureSettings = new ArraySet<>();
     private static final Set<String> sReadableSecureSettings = new ArraySet<>();
     private static final ArrayMap<String, Integer> sReadableSecureSettingsWithMaxTargetSdk =
             new ArrayMap<>();
+    private static final ArrayMap<String, String> sReadableSecureSettingsWithRedactedValue =
+            new ArrayMap<>();
     static {
         Settings.Secure.getPublicSettings(sAllSecureSettings, sReadableSecureSettings,
-                sReadableSecureSettingsWithMaxTargetSdk);
+                sReadableSecureSettingsWithMaxTargetSdk, sReadableSecureSettingsWithRedactedValue);
     }
 
     private static final Set<String> sAllSystemSettings = new ArraySet<>();
     private static final Set<String> sReadableSystemSettings = new ArraySet<>();
     private static final ArrayMap<String, Integer> sReadableSystemSettingsWithMaxTargetSdk =
             new ArrayMap<>();
+    private static final ArrayMap<String, String> sReadableSystemSettingsWithRedactedValue =
+            new ArrayMap<>();
     static {
         Settings.System.getPublicSettings(sAllSystemSettings, sReadableSystemSettings,
-                sReadableSystemSettingsWithMaxTargetSdk);
+                sReadableSystemSettingsWithMaxTargetSdk, sReadableSystemSettingsWithRedactedValue);
     }
 
     private static final Set<String> sAllGlobalSettings = new ArraySet<>();
     private static final Set<String> sReadableGlobalSettings = new ArraySet<>();
     private static final ArrayMap<String, Integer> sReadableGlobalSettingsWithMaxTargetSdk =
             new ArrayMap<>();
+    private static final ArrayMap<String, String> sReadableGlobalSettingsWithRedactedValue =
+            new ArrayMap<>();
     static {
         Settings.Global.getPublicSettings(sAllGlobalSettings, sReadableGlobalSettings,
-                sReadableGlobalSettingsWithMaxTargetSdk);
+                sReadableGlobalSettingsWithMaxTargetSdk, sReadableGlobalSettingsWithRedactedValue);
     }
 
     private final Object mLock = new Object();
@@ -690,7 +699,8 @@ public class SettingsProvider extends ContentProvider {
             case TABLE_GLOBAL -> {
                 if (args.name != null) {
                     Setting setting = getGlobalSetting(args.name);
-                    return packageSettingForQuery(setting, normalizedProjection);
+                    return packageSettingForQuery(setting, normalizedProjection,
+                            sReadableGlobalSettingsWithRedactedValue);
                 } else {
                     return getAllGlobalSettings(projection);
                 }
@@ -699,7 +709,8 @@ public class SettingsProvider extends ContentProvider {
                 final int userId = UserHandle.getCallingUserId();
                 if (args.name != null) {
                     Setting setting = getSecureSetting(args.name, userId, callingDeviceId);
-                    return packageSettingForQuery(setting, normalizedProjection);
+                    return packageSettingForQuery(setting, normalizedProjection,
+                            sReadableSecureSettingsWithRedactedValue);
                 } else {
                     return getAllSecureSettings(userId, Context.DEVICE_ID_DEFAULT, projection);
                 }
@@ -708,7 +719,8 @@ public class SettingsProvider extends ContentProvider {
                 final int userId = UserHandle.getCallingUserId();
                 if (args.name != null) {
                     Setting setting = getSystemSetting(args.name, userId, callingDeviceId);
-                    return packageSettingForQuery(setting, normalizedProjection);
+                    return packageSettingForQuery(setting, normalizedProjection,
+                            sReadableSystemSettingsWithRedactedValue);
                 } else {
                     return getAllSystemSettings(userId, Context.DEVICE_ID_DEFAULT, projection);
                 }
@@ -1579,8 +1591,14 @@ public class SettingsProvider extends ContentProvider {
                     // Caller doesn't have permission to read this setting
                     continue;
                 }
+
                 Setting setting = settingsState.getSettingLocked(name);
-                appendSettingToCursor(result, setting);
+                if (setting == null || setting.isNull()) {
+                    continue;
+                }
+                String value = getEffectiveValue(setting, sReadableGlobalSettingsWithRedactedValue);
+                appendSettingToCursor(result, String.valueOf(setting.getId()), setting.getName(),
+                        value, String.valueOf(setting.isValuePreservedInRestore()));
             }
 
             return result;
@@ -1776,7 +1794,14 @@ public class SettingsProvider extends ContentProvider {
                     setting = mSettingsRegistry.getSettingLocked(SETTINGS_TYPE_SECURE, owningUserId,
                             deviceId, name);
                 }
-                appendSettingToCursor(result, setting);
+
+                if (setting == null || setting.isNull()) {
+                    continue;
+                }
+
+                String value = getEffectiveValue(setting, sReadableSecureSettingsWithRedactedValue);
+                appendSettingToCursor(result, String.valueOf(setting.getId()), setting.getName(),
+                        value, String.valueOf(setting.isValuePreservedInRestore()));
             }
 
             return result;
@@ -2046,7 +2071,12 @@ public class SettingsProvider extends ContentProvider {
 
                 Setting setting = mSettingsRegistry.getSettingLocked(
                         SETTINGS_TYPE_SYSTEM, owningUserId, deviceId, name);
-                appendSettingToCursor(result, setting);
+                if (setting == null || setting.isNull()) {
+                    continue;
+                }
+                String value = getEffectiveValue(setting, sReadableSystemSettingsWithRedactedValue);
+                appendSettingToCursor(result, String.valueOf(setting.getId()), setting.getName(),
+                        value, String.valueOf(setting.isValuePreservedInRestore()));
             }
 
             return result;
@@ -2436,6 +2466,31 @@ public class SettingsProvider extends ContentProvider {
         // Don't enforce the instant app allowlist for now -- its too prone to unintended breakage
         // in the current form.
         return mSettingsRegistry.getSettingsNamesLocked(settingsType, userId, deviceId);
+    }
+
+    private static String getEffectiveValue(Setting setting,
+            @Nullable ArrayMap<String, String> redactedSettingsMap) {
+        if (setting == null || setting.isNull()) {
+            return null;
+        }
+
+        if (!ENABLE_REDACTED_VALUE_FOR_READABLE) {
+            return setting.getValue();
+        }
+
+        if (redactedSettingsMap == null || redactedSettingsMap.isEmpty()) {
+            return setting.getValue();
+        }
+
+        if (UserHandle.getAppId(Binder.getCallingUid()) < Process.FIRST_APPLICATION_UID) {
+            return setting.getValue();
+        }
+
+        String redactedValue = redactedSettingsMap.get(setting.getName());
+        if (redactedValue != null && !redactedValue.isEmpty()) {
+            return redactedValue;
+        }
+        return setting.getValue();
     }
 
     private void enforceSettingReadable(String settingName, int settingsType, int userId) {
@@ -2831,8 +2886,10 @@ public class SettingsProvider extends ContentProvider {
             return Bundle.forPair(Settings.NameValueTable.VALUE, setting.getValue());
         }
         Bundle result = new Bundle();
-        result.putString(Settings.NameValueTable.VALUE,
-                (setting != null && !setting.isNull()) ? setting.getValue() : null);
+        ArrayMap<String, String> redactedSettingsMap = getRedactedSettingsMap(type);
+
+        String value = getEffectiveValue(setting, redactedSettingsMap);
+        result.putString(Settings.NameValueTable.VALUE, value);
 
         synchronized (mLock) {
             if ((setting != null && !setting.isNull()) || isSettingPreDefined(name, type)) {
@@ -2846,6 +2903,20 @@ public class SettingsProvider extends ContentProvider {
             }
         }
         return result;
+    }
+
+    @Nullable
+    private static ArrayMap<String, String> getRedactedSettingsMap(int type) {
+        ArrayMap<String, String> redactedSettingsMap = null;
+
+        if (type == SETTINGS_TYPE_GLOBAL && ENABLE_REDACTED_VALUE_FOR_READABLE) {
+            redactedSettingsMap = sReadableGlobalSettingsWithRedactedValue;
+        } else if (type == SETTINGS_TYPE_SECURE && ENABLE_REDACTED_VALUE_FOR_READABLE) {
+            redactedSettingsMap = sReadableSecureSettingsWithRedactedValue;
+        } else if (type == SETTINGS_TYPE_SYSTEM && ENABLE_REDACTED_VALUE_FOR_READABLE) {
+            redactedSettingsMap = sReadableSystemSettingsWithRedactedValue;
+        }
+        return redactedSettingsMap;
     }
 
     private boolean isSettingPreDefined(String name, int type) {
@@ -3041,12 +3112,16 @@ public class SettingsProvider extends ContentProvider {
         throw new IllegalArgumentException("Invalid URI:" + uri);
     }
 
-    private static MatrixCursor packageSettingForQuery(Setting setting, String[] projection) {
+    private static MatrixCursor packageSettingForQuery(Setting setting, String[] projection,
+            ArrayMap<String, String> redactedSettingsMap) {
         if (setting.isNull()) {
             return new MatrixCursor(projection, 0);
         }
+
         MatrixCursor cursor = new MatrixCursor(projection, 1);
-        appendSettingToCursor(cursor, setting);
+        String value = getEffectiveValue(setting, redactedSettingsMap);
+        appendSettingToCursor(cursor, String.valueOf(setting.getId()), setting.getName(), value,
+                String.valueOf(setting.isValuePreservedInRestore()));
         return cursor;
     }
 
@@ -3066,10 +3141,8 @@ public class SettingsProvider extends ContentProvider {
         return projection;
     }
 
-    private static void appendSettingToCursor(MatrixCursor cursor, Setting setting) {
-        if (setting == null || setting.isNull()) {
-            return;
-        }
+    private static void appendSettingToCursor(MatrixCursor cursor, String settingId,
+            String settingName, String settingValue, String isValuePreservedInRestore) {
         final int columnCount = cursor.getColumnCount();
 
         String[] values =  new String[columnCount];
@@ -3079,16 +3152,16 @@ public class SettingsProvider extends ContentProvider {
 
             switch (column) {
                 case Settings.NameValueTable._ID -> {
-                    values[i] = String.valueOf(setting.getId());
+                    values[i] = settingId;
                 }
                 case Settings.NameValueTable.NAME -> {
-                    values[i] = setting.getName();
+                    values[i] = settingName;
                 }
                 case Settings.NameValueTable.VALUE -> {
-                    values[i] = setting.getValue();
+                    values[i] = settingValue;
                 }
                 case Settings.NameValueTable.IS_PRESERVED_IN_RESTORE -> {
-                    values[i] = String.valueOf(setting.isValuePreservedInRestore());
+                    values[i] = isValuePreservedInRestore;
                 }
             }
         }
